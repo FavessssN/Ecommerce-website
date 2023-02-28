6d5b23398bac1304e3666fd7f74eb217420e45b4
FavessssN <116467808+FavessssN@users.noreply.github.com>
2023-02-28 13:14:53 +0100

    Initial commit

---
+++ ecommerce.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+  if (username === "user" && password === "web_dev") {
+    alert("You have successfully logged in.");
+    location.reload();
+  } else{
+    loginErrorMsg.style.opacity = 1;
+  }
+})
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
